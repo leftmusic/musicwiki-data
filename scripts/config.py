0a7e7960f2b5453d4a2b36760edbc120f837c309
ZuoAnzhi <zuo_ange@icloud.com>
2026-05-24 20:29:18 +0800

    Ignore scripts directory in git

--- scripts/config.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATA_DIR = os.path.join(BASE_DIR, 'data')
-INDEX_FILE = os.path.join(DATA_DIR, 'index.json')
-SONGS_DIR = os.path.join(DATA_DIR, 'songs')
-ARTISTS_DIR = os.path.join(DATA_DIR, 'artists')
-ALBUMS_DIR = os.path.join(DATA_DIR, 'albums')
